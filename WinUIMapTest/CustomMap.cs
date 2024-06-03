cc269c13e30dffdd9058a6403f264fa9d7ec6a57
andreas.wegener@cap-it.de <andreas.wegener>
2024-06-03 11:54:46 +0200

    initail commit

---
+++ WinUIMapTest/CustomMap.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Maui.Controls.Maps;
+
+namespace WinUIMapTest
+{
+    public class CustomMap: Map
+    {
+    }
+}
